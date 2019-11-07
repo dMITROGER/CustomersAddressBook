92a81cfd98951b28f41b8f358253a4a312140669
Dimitri Geras <dmitro.geras@gmail.com>
2019-11-06 19:16:31 -0500

    Customer address book commit

---
+++ CRUDSQLServer/CRUDSQLServer/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace CRUDSQLServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.labelCustomerID = new System.Windows.Forms.Label();
+            this.txbCustomerID = new System.Windows.Forms.TextBox();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelFullName = new System.Windows.Forms.Label();
+            this.txbAddress = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbFullName = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.customerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txbSearch = new System.Windows.Forms.TextBox();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1112, 876);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 47);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // labelCustomerID
+            // 
+            this.labelCustomerID.AutoSize = true;
+            this.labelCustomerID.Location = new System.Drawing.Point(31, 36);
+            this.labelCustomerID.Name = "labelCustomerID";
+            this.labelCustomerID.Size = new System.Drawing.Size(171, 32);
+            this.labelCustomerID.TabIndex = 1;
+            this.labelCustomerID.Text = "Customer ID";
+            // 
+            // txbCustomerID
+            // 
+            this.txbCustomerID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "CustomerID", true));
+            this.txbCustomerID.Location = new System.Drawing.Point(305, 30);
+            this.txbCustomerID.Name = "txbCustomerID";
+            this.txbCustomerID.Size = new System.Drawing.Size(379, 38);
+            this.txbCustomerID.TabIndex = 1;
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataSource = typeof(CRUDSQLServer.Customer);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.labelAddress);
+            this.panel.Controls.Add(this.labelEmail);
+            this.panel.Controls.Add(this.labelFullName);
+            this.panel.Controls.Add(this.labelCustomerID);
+            this.panel.Controls.Add(this.txbAddress);
+            this.panel.Controls.Add(this.txbEmail);
+            this.panel.Controls.Add(this.txbFullName);
+            this.panel.Controls.Add(this.txbCustomerID);
+            this.panel.Location = new System.Drawing.Point(32, 25);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1210, 416);
+            this.panel.TabIndex = 0;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(31, 269);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(119, 32);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(31, 183);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(87, 32);
+            this.labelEmail.TabIndex = 1;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelFullName
+            // 
+            this.labelFullName.AutoSize = true;
+            this.labelFullName.Location = new System.Drawing.Point(31, 108);
+            this.labelFullName.Name = "labelFullName";
+            this.labelFullName.Size = new System.Drawing.Size(144, 32);
+            this.labelFullName.TabIndex = 1;
+            this.labelFullName.Text = "Full Name";
+            // 
+            // txbAddress
+            // 
+            this.txbAddress.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Address", true));
+            this.txbAddress.Location = new System.Drawing.Point(305, 263);
+            this.txbAddress.Multiline = true;
+            this.txbAddress.Name = "txbAddress";
+            this.txbAddress.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbAddress.Size = new System.Drawing.Size(868, 124);
+            this.txbAddress.TabIndex = 4;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Email", true));
+            this.txbEmail.Location = new System.Drawing.Point(305, 177);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(868, 38);
+            this.txbEmail.TabIndex = 3;
+            // 
+            // txbFullName
+            // 
+            this.txbFullName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "FullName", true));
+            this.txbFullName.Location = new System.Drawing.Point(305, 102);
+            this.txbFullName.Name = "txbFullName";
+            this.txbFullName.Size = new System.Drawing.Size(868, 38);
+            this.txbFullName.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customerIDDataGridViewTextBoxColumn,
+            this.fullNameDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.customerBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(32, 521);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 40;
+            this.dataGridView.Size = new System.Drawing.Size(1210, 330);
+            this.dataGridView.TabIndex = 2;
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
+            // 
+            // customerIDDataGridViewTextBoxColumn
+            // 
+            this.customerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.Name = "customerIDDataGridViewTextBoxColumn";
+            this.customerIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fullNameDataGridViewTextBoxColumn
+            // 
+            this.fullNameDataGridViewTextBoxColumn.DataPropertyName = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.HeaderText = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.Name = "fullNameDataGridViewTextBoxColumn";
+            this.fullNameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.fullNameDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.ReadOnly = true;
+            this.addressDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // txbSearch
+            // 
+            this.txbSearch.Location = new System.Drawing.Point(337, 464);
+            this.txbSearch.Name = "txbSearch";
+            this.txbSearch.Size = new System.Drawing.Size(868, 38);
+            this.txbSearch.TabIndex = 1;
+            this.txbSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbSearch_KeyPress);
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Location = new System.Drawing.Point(63, 470);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(105, 32);
+            this.labelSearch.TabIndex = 0;
+            this.labelSearch.Text = "Search";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(976, 876);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 47);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(840, 876);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(130, 47);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(704, 876);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(130, 47);
+            this.btnNew.TabIndex = 3;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1288, 962);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txbSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label labelCustomerID;
+        private System.Windows.Forms.TextBox txbCustomerID;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelFullName;
+        private System.Windows.Forms.TextBox txbAddress;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbFullName;
+        private System.Windows.Forms.TextBox txbSearch;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ CRUDSQLServer/CRUDSQLServer/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDSQLServer
+{
+    public partial class Form1 : Form
+    {
+        TestEntities testEntities;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // search box function (works after pressing Enter in keyboard)
+        private void txbSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (string.IsNullOrEmpty(txbSearch.Text))
+                {
+                    dataGridView.DataSource = customerBindingSource;
+                }
+                else
+                {
+                        var query = from o in customerBindingSource.DataSource as List<Customer>
+                                    where o.CustomerID.CompareTo(txbSearch.Text.Trim()) >= 0 || 
+                                    o.FullName.Contains(txbSearch.Text.Trim()) ||
+                                    o.Address.Contains(txbSearch.Text.Trim())
+                                    select o;
+                   
+                        dataGridView.DataSource = query.ToList();
+                    
+                }
+            }
+        }
+
+        // adding new customer funtion
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                txbCustomerID.Focus();
+                Customer c = new Customer();
+                testEntities.Customers.Add(c);
+                customerBindingSource.Add(c);
+                customerBindingSource.MoveLast();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //edit selected customer function
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+        }
+
+        // cancel funtion
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = false;
+            customerBindingSource.ResetBindings(false);
+            foreach (DbEntityEntry entry in testEntities.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                }
+
+            }
+            dataGridView.DataSource = customerBindingSource;
+
+        }
+
+        // saving function
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                customerBindingSource.EndEdit();
+                testEntities.SaveChangesAsync();
+                panel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                customerBindingSource.ResetBindings(false);
+            }
+        }
+
+        //loading form and populating datagrid with data from database
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new System.Drawing.Size(500, 460);
+            panel.Enabled = false;
+            testEntities = new TestEntities();
+            customerBindingSource.DataSource = testEntities.Customers.ToList();
+        }
+
+        // populating datagrid with data from database
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Are you sure that you want to delete this record?", "Message",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    testEntities.Customers.Remove(customerBindingSource.Current as Customer);
+                    customerBindingSource.RemoveCurrent();
+                }
+            }
+        }
+    }
+}
